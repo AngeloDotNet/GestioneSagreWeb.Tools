d6a1d7eaed00684dc13e3fc785eb0a6708fdedbd
Angelo Pirola <angelo9871@gmail.com>
2023-08-20 16:48:37 +0200

    Refactoring GestioneSagre EfCore

---
+++ GestioneSagre.EFCore/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using GestioneSagre.EFCore.GenericRepository.Interfaces;
+
+namespace GestioneSagre.EFCore.UnitOfWork.Interfaces;
+
+public interface IUnitOfWork<TEntity> : IDisposable where TEntity : class, new()
+{
+    IRepository<TEntity> Repository { get; }
+}
