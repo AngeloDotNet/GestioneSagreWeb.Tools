73d77a7f7c0ffbd645d189b5a269087df51a92b4
Angelo Pirola <angelo9871@gmail.com>
2023-09-22 00:26:27 +0200

    Aggiunta configurazione Options progetto EFCore

---
+++ GestioneSagre.EFCore/Models/Options/ItemsCountOptions.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace GestioneSagre.EFCore.Models.Options;
+
+public class ItemsCountOptions<T>
+{
+    public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
+    public Expression<Func<T, bool>> ConditionWhere { get; set; }
+}
---
+++ GestioneSagre.EFCore/Models/Options/OrderedItems.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+using GestioneSagre.EFCore.Models.Enums;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace GestioneSagre.EFCore.Models.Options;
+
+public class OrderedItems<T>
+{
+    public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
+    public Expression<Func<T, bool>> ConditionWhere { get; set; }
+    public Expression<Func<T, dynamic>> OrderBy { get; set; }
+    public OrderType OrderType { get; set; }
+}
