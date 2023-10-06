b2d7222716cfd929a25936f2357c589635e4a178
Angelo Pirola <angelo9871@gmail.com>
2023-10-07 00:11:02 +0200

    Aggiunti metodi EFCore per Unit ed Integration Test

---
+++ GestioneSagre.EFCore/EFCoreRepository/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace GestioneSagre.EFCore.EFCoreRepository.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class, new()
+{
+    Task<List<TEntity>> GetItemsAsync();
+
+    Task CreateAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task DeleteAsync(TEntity entity);
+}
---
+++ GestioneSagre.EFCore/EFCoreRepository/Repository.cs
@@ -0,0 +1,41 @@
+﻿using GestioneSagre.EFCore.EFCoreRepository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestioneSagre.EFCore.EFCoreRepository;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, new()
+{
+    public DbContext DbContext { get; }
+
+    public Repository(DbContext dbContext)
+    {
+        DbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<List<TEntity>> GetItemsAsync() => await DbContext.Set<TEntity>().AsNoTracking().ToListAsync();
+
+    public async Task CreateAsync(TEntity entity)
+    {
+        DbContext.Set<TEntity>().Add(entity);
+
+        await DbContext.SaveChangesAsync();
+
+        DbContext.Entry(entity).State = EntityState.Detached;
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        DbContext.Set<TEntity>().Update(entity);
+
+        await DbContext.SaveChangesAsync();
+
+        DbContext.Entry(entity).State = EntityState.Detached;
+    }
+
+    public async Task DeleteAsync(TEntity entity)
+    {
+        DbContext.Set<TEntity>().Remove(entity);
+
+        await DbContext.SaveChangesAsync();
+    }
+}
