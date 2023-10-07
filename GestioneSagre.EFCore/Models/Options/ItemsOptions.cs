e6ef532639d3bcb417f27f855daf4565a4f13a99
Angelo Pirola <angelo9871@gmail.com>
2023-10-08 00:13:59 +0200

    Fix Models Options

--- GestioneSagre.EFCore/Models/Options/ItemsOptions.cs
+++ GestioneSagre.EFCore/Models/Options/ItemsOptions.cs
@@ -1,10 +1,16 @@
 ﻿using System.Linq.Expressions;
+using GestioneSagre.EFCore.Models.Enums;
 using Microsoft.EntityFrameworkCore.Query;
 
 namespace GestioneSagre.EFCore.Models.Options;
 
 public class ItemsOptions<T>
 {
+    //public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
+    //public Expression<Func<T, bool>> ConditionWhere { get; set; }
+
     public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
     public Expression<Func<T, bool>> ConditionWhere { get; set; }
+    public Expression<Func<T, dynamic>> OrderBy { get; set; }
+    public OrderType OrderType { get; set; }
 }
--- GestioneSagre.EFCore/Models/Options/OrderedItems.cs
+++ GestioneSagre.EFCore/Models/Options/OrderedItems.cs
@@ -1,13 +1,9 @@
-﻿using System.Linq.Expressions;
-using GestioneSagre.EFCore.Models.Enums;
-using Microsoft.EntityFrameworkCore.Query;
+﻿namespace GestioneSagre.EFCore.Models.Options;
 
-namespace GestioneSagre.EFCore.Models.Options;
-
-public class OrderedItems<T>
-{
-    public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
-    public Expression<Func<T, bool>> ConditionWhere { get; set; }
-    public Expression<Func<T, dynamic>> OrderBy { get; set; }
-    public OrderType OrderType { get; set; }
-}
+//public class OrderedItems<T>
+//{
+//    public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
+//    public Expression<Func<T, bool>> ConditionWhere { get; set; }
+//    public Expression<Func<T, dynamic>> OrderBy { get; set; }
+//    public OrderType OrderType { get; set; }
+//}
