268c2ea13c0b6d469e844549009919791d58de35
Angelo Pirola <angelo9871@gmail.com>
2023-10-09 11:05:11 +0200

    Code cleanup Models Options

--- GestioneSagre.EFCore/Models/Options/ItemsOptions.cs
+++ GestioneSagre.EFCore/Models/Options/ItemsOptions.cs
@@ -6,9 +6,6 @@ namespace GestioneSagre.EFCore.Models.Options;
 
 public class ItemsOptions<T>
 {
-    //public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
-    //public Expression<Func<T, bool>> ConditionWhere { get; set; }
-
     public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
     public Expression<Func<T, bool>> ConditionWhere { get; set; }
     public Expression<Func<T, dynamic>> OrderBy { get; set; }
--- GestioneSagre.EFCore/Models/Options/OrderedItems.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestioneSagre.EFCore.Models.Options;
-
-//public class OrderedItems<T>
-//{
-//    public Func<IQueryable<T>, IIncludableQueryable<T, object>> Includes { get; set; }
-//    public Expression<Func<T, bool>> ConditionWhere { get; set; }
-//    public Expression<Func<T, dynamic>> OrderBy { get; set; }
-//    public OrderType OrderType { get; set; }
-//}
