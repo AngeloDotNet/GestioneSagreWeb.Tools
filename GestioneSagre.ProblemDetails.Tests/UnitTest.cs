34396d5da24957c77c1bd77594c61b029b16b275
Angelo Pirola <angelo9871@gmail.com>
2023-10-13 09:02:34 +0200

    Aggiornati Unit Test Problem Details

--- GestioneSagre.ProblemDetails.Tests/UnitTest.cs
+++ GestioneSagre.ProblemDetails.Tests/UnitTest.cs
@@ -24,6 +24,9 @@ public class UnitTest
     private static NotFoundException CreateNotFoundException() => new NotFoundException(message: "Not Found Exception");
     private static BadRequestException CreateBadRequestException() => new BadRequestException(message: "Bad Request Exception");
     private static NotModifiedException CreateNotModifiedException() => new NotModifiedException(message: "Not Modified Exception");
+    private static NotAcceptableException CreateNotAcceptableException() => new NotAcceptableException("Not Acceptable Exception");
+    private static ConflictException CreateConflictException() => new ConflictException("Conflict Exception");
+    private static UnprocessableEntityException CreateUnprocessableEntityException() => new UnprocessableEntityException("Unprocessable Entity Exception");
 
     [Test]
     public void Should_Throw_NotFoundException()
@@ -48,6 +51,33 @@ public class UnitTest
     {
         expectedErrorMessage = "Not Modified Exception";
         var exception = Assert.Throws<NotModifiedException>(() => throw CreateNotModifiedException());
+
+        Assert.That(exception.Message, Is.EqualTo(expectedErrorMessage));
+    }
+
+    [Test]
+    public void Should_Throw_NotAcceptableException()
+    {
+        expectedErrorMessage = "Not Acceptable Exception";
+        var exception = Assert.Throws<NotAcceptableException>(() => throw CreateNotAcceptableException());
+
+        Assert.That(exception.Message, Is.EqualTo(expectedErrorMessage));
+    }
+
+    [Test]
+    public void Should_Throw_ConflictException()
+    {
+        expectedErrorMessage = "Conflict Exception";
+        var exception = Assert.Throws<ConflictException>(() => throw CreateConflictException());
+
+        Assert.That(exception.Message, Is.EqualTo(expectedErrorMessage));
+    }
+
+    [Test]
+    public void Should_Throw_UnprocessableEntityException()
+    {
+        expectedErrorMessage = "Unprocessable Entity Exception";
+        var exception = Assert.Throws<UnprocessableEntityException>(() => throw CreateUnprocessableEntityException());
 
         Assert.That(exception.Message, Is.EqualTo(expectedErrorMessage));
     }
