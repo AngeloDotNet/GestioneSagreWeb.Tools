8ded406c486d9ca34b35da5835e17349c695bb86
Angelo Pirola <angelo9871@gmail.com>
2023-10-18 08:55:00 +0200

    Aggiunti due nuovi metodi formato lista

--- GestioneSagre.Shared/Extensions/RegisterServices.cs
+++ GestioneSagre.Shared/Extensions/RegisterServices.cs
@@ -23,6 +23,22 @@ public static class RegisterServices
         return services;
     }
 
+    public static IServiceCollection AddDefaultOperationResultFormatList(this IServiceCollection services)
+    {
+        services.AddOperationResult(options =>
+        {
+            options.ErrorResponseFormat = ErrorResponseFormat.List;
+
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotModified, StatusCodes.Status304NotModified);
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotAcceptable, StatusCodes.Status406NotAcceptable);
+            options.StatusCodesMapping.Add(CustomFailureReasons.UnprocessableEntity, StatusCodes.Status422UnprocessableEntity);
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotImplemented, StatusCodes.Status501NotImplemented);
+        },
+        validationErrorDefaultMessage: "Errors occurred");
+
+        return services;
+    }
+
     public static IServiceCollection AddOperationResultHttpStatusCodes(this IServiceCollection services)
     {
         services.AddOperationResult(options =>
@@ -41,19 +57,35 @@ public static class RegisterServices
         return services;
     }
 
+    public static IServiceCollection AddOperationResultHttpStatusCodesFormatList(this IServiceCollection services)
+    {
+        services.AddOperationResult(options =>
+        {
+            options.ErrorResponseFormat = ErrorResponseFormat.List;
+
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotModified, StatusCodes.Status304NotModified);
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotAcceptable, StatusCodes.Status406NotAcceptable);
+            options.StatusCodesMapping.Add(CustomFailureReasons.UnprocessableEntity, StatusCodes.Status422UnprocessableEntity);
+            options.StatusCodesMapping.Add(CustomFailureReasons.NotImplemented, StatusCodes.Status501NotImplemented);
+
+            options.UseHttpStatusCodes = true;
+        },
+        validationErrorDefaultMessage: "Errors occurred");
+
+        return services;
+    }
+
     // Use this if you need to dinamically change the message, i.e., based on user culture.
     //public static IServiceCollection AddOperationResultValidationError(this IServiceCollection services)
     //{
     //    services.AddOperationResult(options =>
     //    {
-    //        options.ErrorResponseFormat = ErrorResponseFormat.Default;
-
     //        options.StatusCodesMapping.Add(CustomFailureReasons.NotModified, StatusCodes.Status304NotModified);
     //        options.StatusCodesMapping.Add(CustomFailureReasons.NotAcceptable, StatusCodes.Status406NotAcceptable);
     //        options.StatusCodesMapping.Add(CustomFailureReasons.UnprocessableEntity, StatusCodes.Status422UnprocessableEntity);
     //        options.StatusCodesMapping.Add(CustomFailureReasons.NotImplemented, StatusCodes.Status501NotImplemented);
     //    },
-    //    //updateModelStateResponseFactory: true);
+
     //    validationErrorMessageProvider: (state) => Messages.ValidationErrorMessage);
 
     //    return services;
