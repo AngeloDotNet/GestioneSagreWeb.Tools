4aa10db2e8cfa704e267bad173c501a7952f861a
Angelo Pirola <angelo9871@gmail.com>
2023-11-08 19:15:00 +0100

    Rimossa configurazione DateOnly e TimeOnly

--- GestioneSagre.EFCore/ValueConverter/DateOnlyConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-
-namespace GestioneSagre.EFCore.ValueConverter;
-
-public class DateOnlyConverter : ValueConverter<DateOnly, DateTime>
-{
-    public DateOnlyConverter() : base(d => d.ToDateTime(TimeOnly.MinValue), d => DateOnly.FromDateTime(d))
-    { }
-}
